c53976231e49744e1764f4ee1164dd538c82b10c
basketball20182019 <46387123+basketball20182019@users.noreply.github.com>
2019-01-04 14:49:10 -0800

    Add files via upload

---
+++ Web/typed/type.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'Love',
+      'Pixel gun 3d',
+      'with my friends!'
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'Pixel gun 3d!!!!!!!!!!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'Try fruit ninja🍊🔪🥥🍋',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'cool guns and swords!!!!🗡🗡🔫🔫',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'Here is another gun.',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
